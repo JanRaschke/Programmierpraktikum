9329b84c7f586371218b44c9a9676bb4383d4ef4
Nils <nils.gellert@outlook.de>
2026-08-17 13:30:13 +0200

    Implement dynamic programming algorithm for problem 2

--- src/problem2.java
+++ src/problem2.java
@@ -6,13 +6,32 @@ public class problem2 {
             return;
         }
 
-        // read number of N integers
         int n = StdIn.readInt();
         int[] input = new int[n];
         for (int i = 0; i < n; i++) {
             input[i] = StdIn.readInt();
         }
-        
 
+
+        int[][] table = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            table[i][i] = input[i];
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                int maxVal = Integer.MIN_VALUE;
+                for (int k = i; k < j; k++) {
+                    int val = (table[i][k] + table[k + 1][j]) / 2;
+                    if (val > maxVal) {
+                        maxVal = val;
+                    }
+                }
+                table[i][j] = maxVal;
+            }
+        }
+
+        StdOut.println(table[0][n - 1]);
     }
 }
