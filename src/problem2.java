33c14c1916817f780a6e18e0c96665c97fb1cbfa
JanRaschke <janphilipraschke@gmail.com>
2026-08-17 11:07:07 +0200

    Implement read of input.

---
+++ src/problem2.java
@@ -0,0 +1,18 @@
+import edu.princeton.cs.algs4.*;
+
+public class problem2 {
+    public static void main(String[] args) {
+        if (StdIn.isEmpty()) {
+            return;
+        }
+
+        // read number of N integers
+        int n = StdIn.readInt();
+        int[] input = new int[n];
+        for (int i = 0; i < n; i++) {
+            input[i] = StdIn.readInt();
+        }
+        
+
+    }
+}
